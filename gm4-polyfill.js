52011741636b7bbe387840def0b9fa0faa0cc914
Stig Nygaard <StigNygaard@users.noreply.github.com>
2017-11-05 09:28:43 +0100

    Don't replace an existing menu, but extend it...
    If the page (or another userscript) had already defined a context-menu on body (with an id different from 'gm-registered-menu'), the old version of registerMenuCommand would delete/replace the existing menu. This version always re-uses (extends) an existing context-menu on body element, no matter what id it was given.

--- gm4-polyfill.js
+++ gm4-polyfill.js
@@ -52,7 +52,10 @@ if (typeof GM_registerMenuCommand == 'undefined') {
       console.error('GM_registerMenuCommand got no body.');
       return;
     }
-    let menu = document.getElementById('gm-registered-menu');
+    let menu = null;
+    if (document.body.getAttribute('contextmenu')) {
+        menu = document.querySelector('menu#'+document.body.getAttribute('contextmenu'));
+    }
     if (!menu) {
       menu = document.createElement('menu')
       menu.setAttribute('id', 'gm-registered-menu');
@@ -60,6 +63,7 @@ if (typeof GM_registerMenuCommand == 'undefined') {
       document.body.appendChild(menu);
       document.body.setAttribute('contextmenu', 'gm-registered-menu');
     }
+    document.body.setAttribute('contextmenu', menu.getAttribute('id'));
     let menuItem = document.createElement('menuitem');
     menuItem.textContent = caption;
     menuItem.addEventListener('click', commandFunc, true);
